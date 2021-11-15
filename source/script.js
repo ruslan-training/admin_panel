9ffa8062c70b3beb62ac2d51e88c2ac7e5cb3582
kbnd <devkbnd@gmail.com>
2021-11-15 19:35:57 +0200

    full admin panel

--- source/script.js
+++ source/script.js
@@ -1,123 +1,530 @@
-// HEADER
-window.onscroll = function showHeader() {
-    let header = document.querySelector('.header');
-    if(window.scrollY > 500) {
-      header.classList.add('menu-fixed');
-    } else {
-      header.classList.remove('menu-fixed');
-    }
-  }
 
-
-// Burger
-
-let burger = document.querySelector('.burger'),
-    adaptivemenu = document.querySelector('.adaptive-menu'),
-    lists = document.querySelectorAll('.list__item'),
-    body = document.body;
-
-burger.addEventListener('click', function(){
-    burger.classList.toggle('_active');
-    adaptivemenu.classList.toggle('_active');
-    body.classList.toggle('_lock');
-    for(list of lists) {
-        list.classList.toggle('_active');
-    }
-})
-
-let productBoxes = document.querySelectorAll('.product-box'),
-    productBoxBtns = document.querySelector('.btn'),
-    productBoxIcons = document.querySelector('.product-box__icon');
-
-    for (let productBox of productBoxes) {
-        productBox.addEventListener('mouseenter', function(){
-            productBox.classList.add('_active');
-        })
-        productBox.addEventListener('mouseleave', function(){
-            productBox.classList.remove('_active');
-        }) 
-    }
-
-//  Slider testimonials
-
-$('.testimonials-slider-wrapper').slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    speed: 1500,
-    autoplay: 1,
-    Infinity: true,
-    pauseOnHover: true,
-    waitForAnimate: false,
-
-    responsive: [{
-        breakpoint: 600,
-        settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-    }
-}]
-});
-
-$('.shop__products').slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    speed: 1500,
-    autoplay: 0,
-    Infinity: true,
-    waitForAnimate: true,
-
-    responsive: [   
-    {
-        breakpoint: 900,
-        settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
+new Swiper('.slider-wrapper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
     },
-        breakpoint: 600,
-        settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-        }
-    }
-]
+    // Навигация 
+	// Буллеты, текущее положение, прогрессбар
+	pagination: {
+		el: '.swiper-pagination',
+		/*
+		// Буллеты
+		type: 'bullets',
+		clickable: true,
+		// Динамические буллеты
+		dynamicBullets: true,
+		// Кастомные буллеты
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + (index + 1) + '</span>';
+		},
+		*/
+		/*
+		// Фракция
+		type: 'fraction',
+		// Кастомный вывод фракции
+		renderFraction: function (currentClass, totalClass) {
+			return 'Фото <span class="' + currentClass + '"></span>' +
+				' из ' +
+				'<span class="' + totalClass + '"></span>';
+		},
+		*/
+		// Прогрессбар
+		//type: 'progressbar'
+	},
+	// Скролл
+	/*
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		// Возможность перетаскивать скролл
+		draggable: true
+	},
+	*/
 
+	// Включение/отключение
+	// перетаскивания на ПК
+	simulateTouch: true,
+	// Чувствительность свайпа
+	touchRatio: 1,
+	// Угол срабатывания свайпа/перетаскивания
+	touchAngle: 45,
+	// Курсор перетаскивания
+	grabCursor: true,
+
+	// Переключение при клике на слайд
+	slideToClickedSlide: false,
+
+	// Навигация по хешу
+	hashNavigation: {
+		// Отслеживать состояние
+		watchState: true,
+	},
+
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// // Управление колесом мыши
+	// mousewheel: {
+	// 	// Чувствительность колеса мыши
+	// 	sensitivity: 1,
+	// 	// Класс объекта на котором
+	// 	// будет срабатывать прокрутка мышью.
+	// 	//eventsTarget: ".image-slider"
+	// },
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 3,
+
+	// Отключение функционала
+	// если слайдов меньше чем нужно
+	watchOverflow: true,
+
+	// Отступ между слайдами
+	spaceBetween: 30,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Активный слайд по центру
+	centeredSlides: false,
+
+	// Стартовый слайд.
+	initialSlide: 0,
+
+	// Мультирядность
+	slidesPerColumn: 1,
+
+	// Бесконечный слайдер
+	loop: true,
+
+	// Кол-во дублирующих слайдов
+	loopedSlides: 0,
+
+	// Свободный режим
+	freeMode: false,
+
+	// Автопрокрутка
+	/*
+	autoplay: {
+		// Пауза между прокруткой
+		delay: 1000,
+		// Закончить на последнем слайде
+		stopOnLastSlide: true,
+		// Отключить после ручного переключения
+		disableOnInteraction: false
+	},
+	*/
+
+	// Скорость
+	speed: 400,
+
+	// Вертикальный слайдер
+	// direction: 'horizontal',
+
+
+	// Эффекты переключения слайдов.
+	// Листание
+	effect: 'slide',
+
+	/*
+	// Эффекты переключения слайдов.
+	// Cмена прозрачности
+	effect: 'fade',
+
+	// Дополнение к fade
+	fadeEffect: {
+		// Параллельная
+		// смена прозрачности
+		crossFade: true
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Переворот
+	effect: 'flip',
+
+	// Дополнение к flip
+	flipEffect: {
+		// Тень
+		slideShadows: true,
+		// Показ только активного слайда
+		limitRotation: true
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Куб
+	effect: 'cube',
+
+	// Дополнение к cube
+	cubeEffect: {
+		// Настройки тени
+		slideShadows: true,
+		shadow: true,
+		shadowOffset: 20,
+		shadowScale: 0.94
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Эффект потока
+	effect: 'coverflow',
+
+	// Дополнение к coverflow
+	coverflowEffect: {
+		// Угол
+		rotate: 20,
+		// Наложение
+		stretch: 50,
+		// Тень
+		slideShadows: true,
+	},
+	*/
+	
+	// Брейк поинты (адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		600: {
+			slidesPerView: 2,
+		},
+		1055: {
+			slidesPerView: 3,
+		}
+	},
+	
+	/*
+	// Брейк поинты (адаптив)
+	// Соотношение сторон
+	breakpoints: {
+		'@0.75': {
+			slidesPerView: 1,
+		},
+		'@1.00': {
+			slidesPerView: 2,
+		},
+		'@1.50': {
+			slidesPerView: 3,
+		}
+	},
+	*/
+
+	// Отключить предзагрузка картинок
+	preloadImages: false,
+	// Lazy Loading
+	// (подгрузка картинок)
+	lazy: {
+		// Подгружать на старте
+		// переключения слайда
+		loadOnTransitionStart: false,
+		// Подгрузить предыдущую
+		// и следующую картинки
+		loadPrevNext: false,
+	},
+	// Слежка за видимыми слайдами
+	watchSlidesProgress: true,
+	// Добавление класса видимым слайдам
+	watchSlidesVisibility: true,
+
+	// Зум картинки
+	zoom: {
+		// Макимальное увеличение
+		maxRatio: 5,
+		// Минимальное увеличение
+		minRatio: 1,
+	},
+
+	// Миниатюры (превью)
+	/*
+	thumbs: {
+		// Свайпер с мениатюрами
+		// и его настройки
+		swiper: {
+			el: '.image-mini-slider',
+			slidesPerView: 5,
+		}
+	},
+	*/
+	/*
+	// Передача управления
+	controller: {
+		control: myTextSlider
+	},
+	*/
+
+	// Обновить свайпер
+	// при изменении элементов слайдера
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+
+	// Виртуальные слайды
+	// формирование слайдера
+	/*
+	virtual: {
+		slides: (function () {
+			let slide = []
+			for (let i = 0; i < 500; i++) {
+				slide.push(`<div class="image-slider__text">Слайд №${i}</div>`);
+			}
+			return slide;
+		}()),
+	},
+	*/
+	/*
+	// Доступность
+	a11y: {
+		// Включить/выключить
+		enabled: true,
+		// Сообщения
+		prevSlideMessage: 'Previous slide',
+		nextSlideMessage: 'Next slide',
+		firstSlideMessage: 'This is the first slide',
+		lastSlideMessage: 'This is the last slide',
+		paginationBulletMessage: 'Go to slide {{index}}',
+		notificationClass: 'swiper-notification',
+		containerMessage: '',
+		containerRoleDescriptionMessage: '',
+		itemRoleDescriptionMessage: '',
+		// и т.д.
+	},
+	*/
+	/*
+	// События
+	on: {
+		// Событие инициализации
+		init: function () {
+			console.log('Слайдер запущен!');
+		},
+		// Событие смены слайда
+		slideChange: function () {
+			console.log('Слайд переключен');
+		}
+	},
+	*/
 });
 
+/*
+// Слайдер в слайдере
+new Swiper('.image-in-slider', {
+	// Курсор перетаскивания
+	grabCursor: true,
+	// Навигация
+	// пагинация, текущее положение, прогрессбар
+	pagination: {
+		el: '.swiper-pagination',
+		// Буллеты
+		clickable: true,
+	},
+	// Корректная работа
+	// перетаскивания\свайпа
+	// для дочернего слайдера
+	nested: true,
+});
+*/
+/*
+// Еще один слайдер
+let myTextSlider = new Swiper('.text-slider', {
+	// Количество слайдов для показа
+	slidesPerView: 3,
+	// Отступ между слайдами
+	spaceBetween: 30,
+});
+
+// Передача управления
+myImageSlider.controller.control = myTextSlider;
+myTextSlider.controller.control = myImageSlider;
+*/
+
+/*
+// Параллакс слайдер
+new Swiper('.parallax-slider', {
+	// Включаем параллакс
+	parallax: true,
+	// скорость переключения
+	speed: 2000,
+	// Стрелки
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+});
+*/
+
+
+/*
+// Параметры
+// Получение
+let qSlides = myImageSlider.slides.length;
+
+// Изменение
+myImageSlider.params.speed = 3000;
+*/
+
+/*
+// Методы
+// Обновить слайдер
+myImageSlider.update();
+
+// Переключится на слайд 2, скорость 800
+myImageSlider.slideTo(2, 800);
+*/
+
+/*
+// События
+// Событие смены слайда
+myImageSlider.on('slideChange', function () {
+	console.log('Слайд переключен');
+});
+*/
+
+
+/*
+// Запуск автоппрокрутки при наведении
+let sliderBlock = document.querySelector('.image-slider');
+
+// myImageSlider  - это переменная которой присвоен слайдер
+
+sliderBlock.addEventListener("mouseenter", function (e) {
+	myImageSlider.params.autoplay.disableOnInteraction = false;
+	myImageSlider.params.autoplay.delay = 500;
+	myImageSlider.autoplay.start();
+});
+sliderBlock.addEventListener("mouseleave", function (e) {
+	myImageSlider.autoplay.stop();
+});
+
+*/
+
+
+/*
+// Фракция
+let mySliderAllSlides = document.querySelector('.image-slider__total');
+let mySliderCurrentSlide = document.querySelector('.image-slider__current');
+
+mySliderAllSlides.innerHTML = myImageSlider.slides.length;
+
+myImageSlider.on('slideChange', function () {
+	let currentSlide = ++myImageSlider.realIndex;
+	mySliderCurrentSlide.innerHTML = currentSlide;
+});
+*/
+// Hover class active add
+
+let side = document.querySelector('.side');
+
+   {
+        side.addEventListener('mouseenter', function(){
+            side.classList.add('_active');
+        })
+        side.addEventListener('mouseleave', function(){
+            side.classList.remove('_active');
+        }) 
+   }
+
+// Progress bar 
+let number = document.getElementById('number');
+let counter = 0;
+setInterval(() => {
+    if(counter == 45) {
+        clearInterval();
+    } else {
+        counter += 1;
+        number.innerHTML = counter;
+    }
    
-// Scroll Animation
+}, 35);
 
-const animItems = document.querySelectorAll(`._animated`)
-if (animItems.length > 0) {
-    window.addEventListener(`scroll`, animOnScroll)
-
-    function animOnScroll() {
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index]
-            const animItemHeight = animItem.offsetHeight
-            const animItemOffSet = offset(animItem).top
-            const animStart = 4
-            let animItemPoint = window.innerHeight - animItemHeight / animStart
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart
-            }
-            if ((window.scrollY > animItemOffSet - animItemPoint) && window.scrollY < (animItemOffSet + animItemHeight)) {
-                animItem.classList.add(`_active`)
-            } else {
-                if (!(animItem.classList.contains(`_once-animated`))) {
-                    animItem.classList.remove(`_active`)
-                }
-            }
-        }
+let number1 = document.getElementById('number1');
+let counter1 = 0;
+setInterval(() => {
+    if(counter1 >= 7493) {
+        clearInterval();
+    } else {
+        counter1 += 111;
+        number1.innerHTML = counter1;
     }
+   
+}, 24);
 
-    function offset(el) {
-        const rect = el.getBoundingClientRect()
-        let scrollLeft = window.scrollY || document.documentElement.scrollLeft
-        let scrollTop = window.scrollY || document.documentElement.scrollTop
-        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+let number2 = document.getElementById('number2');
+let counter2 = 0;
+setInterval(() => {
+    if(counter2 == 65) {
+        clearInterval();
+    } else {
+        counter2 += 1;
+        number2.innerHTML = counter2;
     }
+   
+}, 25);
 
-    setTimeout(() => {
-        animOnScroll()
-    }, 300)
-}
+let number3 = document.getElementById('number3');
+let counter3 = 0;
+setInterval(() => {
+    if(counter3 >= 23981) {
+        clearInterval();
+    } else {
+        counter3 += 123;
+        number3.innerHTML = counter3;
+    }
+    
+}, 8);
+
+
+// const bar1 = document.getElementById('bar1');
+// const bar2 = document.getElementById('bar2');
+// const bar3 = document.getElementById('bar3');
+// const bar4 = document.getElementById('bar4');
+
+// function progressBar(target, value = 0) {
+//   const progress = document.createElement("progress");
+//   progress.max = 100;
+//   progress.value = 0;
+//   target.appendChild(progress)
+//   return [
+//     () => value,
+//     (add) => {
+//       value += add;
+//       if (value >= 100) {
+//         progress.value = 100;
+//         return;
+//       }
+//       progress.value = value;
+//     },
+//   ];
+// }
+
+// const [progress1, addProgress1] = progressBar(bar1);
+// const [progress2, addProgress2] = progressBar(bar2);
+// const [progress3, addProgress3] = progressBar(bar3);
+// const [progress4, addProgress4] = progressBar(bar4);
+
+// setInterval(() => addProgress1(Math.ceil(Math.random() * 10)), 100)
+// setInterval(() => addProgress2(Math.ceil(Math.random() * 10)), 200)
+// setInterval(() => addProgress3(Math.ceil(Math.random() * 10)), 150)
+// setInterval(() => addProgress4(Math.ceil(Math.random() * 10)), 50)
